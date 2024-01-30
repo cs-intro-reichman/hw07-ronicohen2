6b36192f19304601ae79b37f5b0b1cad994d80e2
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2024-01-30 22:42:35 +0200

    Update SpellChecker.java

--- SpellChecker.java
+++ SpellChecker.java
@@ -4,15 +4,12 @@ public class SpellChecker
 
 
 	public static void main(String[] args) 
-	{
-		String word1 = "hello";
-		String word2 = "hell";
-		System.out.println(levenshtein(word1, word2));
-		//String word = args[0];
-		//int threshold = Integer.parseInt(args[1]);
-		//String[] dictionary = readDictionary("dictionary.txt");
-		//String correction = spellChecker(word, threshold, dictionary);
-		//System.out.println(correction);//
+	{		
+		String word = args[0];
+		int threshold = Integer.parseInt(args[1]);
+		String[] dictionary = readDictionary("dictionary.txt");
+		String correction = spellChecker(word, threshold, dictionary);
+		System.out.println(correction);
 	}
 
 	public static String tail(String str) 
@@ -49,17 +46,42 @@ public class SpellChecker
 	public static String[] readDictionary(String fileName) 
 	{
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i=0; i<3000; i++)
+		{
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) 
 	{
-		// Your code goes here
+		int lev;
+		int min = 0;
+		int minlev = threshold  + 1;
+		boolean found = false;
+		for (int i=0; i < dictionary.length ; i++)
+		{
+			if (dictionary[i].equals(word))
+			{
+				return word;
+			}
+			lev = levenshtein(word, dictionary[i]);
+			if (lev <= threshold)
+			{
+				found = true;
+				if (lev < minlev)
+				{
+					min = i;
+					minlev = lev;
+				}
+			}
+			
+		}
+		if (found)
+		{
+			return dictionary[min];
+		}	
 		return word;
 	}
 
