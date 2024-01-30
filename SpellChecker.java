9e1ecab598baf850aa06950de7486c79e2583ba6
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2024-01-30 21:35:39 +0200

    Update SpellChecker.java

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,24 +1,53 @@
 
-public class SpellChecker {
+public class SpellChecker
+{
 
 
-	public static void main(String[] args) {
-		String word = args[0];
-		int threshold = Integer.parseInt(args[1]);
-		String[] dictionary = readDictionary("dictionary.txt");
-		String correction = spellChecker(word, threshold, dictionary);
-		System.out.println(correction);
+	public static void main(String[] args) 
+	{
+		String word1 = "hello";
+		String word2 = "hell";
+		System.out.println(levenshtein(word1, word2));
+		//String word = args[0];
+		//int threshold = Integer.parseInt(args[1]);
+		//String[] dictionary = readDictionary("dictionary.txt");
+		//String correction = spellChecker(word, threshold, dictionary);
+		//System.out.println(correction);//
 	}
 
-	public static String tail(String str) {
-		// Your code goes here
+	public static String tail(String str) 
+	{
+		if (str.length()==0)
+		{
+			return " ";
+		}
+		else return str.substring(1, str.length());
 	}
 
-	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+	public static int levenshtein(String word1, String word2) 
+	{
+		int lev = 0;
+		if (word1.isEmpty())
+		{
+			lev = word2.length();
+		}
+		else if (word2.isEmpty())
+		{
+			lev =  word1.length();
+		}
+		else if (word1.charAt(0)==word2.charAt(0))
+		{
+			lev = levenshtein(tail(word1), tail(word2));
+		}
+		else 
+		{
+			lev = 1 + Math.min(levenshtein(tail(word1), tail(word2)), Math.min(levenshtein(tail(word1), word2), levenshtein(word1, tail(word2))));
+		}
+		return lev;
 	}
 
-	public static String[] readDictionary(String fileName) {
+	public static String[] readDictionary(String fileName) 
+	{
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
@@ -28,8 +57,10 @@ public class SpellChecker {
 		return dictionary;
 	}
 
-	public static String spellChecker(String word, int threshold, String[] dictionary) {
+	public static String spellChecker(String word, int threshold, String[] dictionary) 
+	{
 		// Your code goes here
+		return word;
 	}
 
 }
